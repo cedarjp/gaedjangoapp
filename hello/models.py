e907834a6bb39400c9b54dc4d32838db7fb61afe
Shinji Sugita <cedarjp.s@gmail.com>
2015-05-10 22:22:40 +0900

    Managed VMs

--- hello/models.py
+++ hello/models.py
@@ -1,6 +1,10 @@
 # -*- coding:utf-8 -*-
 from django.db import models
 
+def cache():
+    from google.appengine.api import memcache
+    return memcache
+
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,11 @@
 
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Hello, world.")
+    from models import cache
+    cache_key = 'counter'
+    counter = cache().get(cache_key)
+    if not counter:
+        counter = 0
+    counter += 1
+    cache().set(cache_key, counter)
+    return HttpResponse("Hello, world. count:{}".format(counter))
